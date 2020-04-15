390538d4adec1b370de5a180d152bfd201d6bbd7
BrandonKutivan <bskutivan@gmail.com>
2020-04-15 14:25:42 -0700

    connected index htm and single repo html pages with error handling for missing query paramaters and no issues.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,26 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+    } document.location.replace("./index.html");
+};
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
 
 var displayIssues = function(issues) {
 
@@ -44,12 +66,17 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("bskutivan/run-buddy");
+getRepoName();
+
